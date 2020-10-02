aa9b5faa20a1f442e89df7fb4806e5ac9bf4430a
Matthew Tullo <mtullo1@binghamton.edu>
2020-10-02 19:32:45 -0400

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 struct gate{
-    //0 = left, 1 = right 2 = center
-    int state;
+    //L = left, R = right C = center
+    string state;
 };
 
 int main(int argc, char *argv[]){
@@ -18,72 +18,66 @@ int main(int argc, char *argv[]){
 
     gate x1, x2, x3, x4;
 
-    if(starting[0] == 'L')
-         x1.state = 0;
-    if(starting[0] == 'R')
-        x1.state = 1;
-    else x1.state = 2;
-    if(starting[1] == 'L')
-        x2.state = 0;
-    else x2.state = 1;
-    if(starting[2] == 'L')
-        x3.state = 0;
-    else x3.state = 1;
-    if(starting[3] == 'L')
-        x4.state = 0;
-    else x4.state = 1;
+    x1.state = starting[0];
+    x2.state = starting[1];
+    x3.state = starting[2];
+    x4.state = starting[4];
 
     for(int i = 0; i<path.length()-1; i++){
         if(path[i] == '0'){
-            if(x1.state == 0){
-                if(x2.state == 0)
-                    x2.state = 1;
-                else x2.state = 0;
-                x1.state = 1;
+            if(x1.state == "L"){
+                if(x2.state == "L")
+                    x2.state = "R";
+                else x2.state = "L";
+                x1.state = "R";
             }
-            else if(x1.state == 1){
-                if(x3.state == 0)
-                    x3.state = 1;
-                else x3.state = 0;
-                x1.state = 2;
+            else if(x1.state == "R"){
+                if(x3.state == "L")
+                    x3.state = "R";
+                else x3.state = "L";
+                x1.state = "C";
             }
-            else if(x1.state == 2){
-                if(x4.state == 0)
-                    x4.state = 1;
-                else x4.state = 0;
-                x1.state = 0;
+            else if(x1.state == "C"){
+                if(x4.state == "L")
+                    x4.state = "R";
+                else x4.state = "L";
+                x1.state = "L";
             }
         }
         else{
-            if(x1.state == 0){
-                if(x2.state == 0)
-                    x2.state = 1;
-                else x2.state = 0;
-                x1.state = 2;
+            if(x1.state == "L"){
+                if(x2.state == "L")
+                    x2.state = "R";
+                else x2.state = "L";
+                x1.state = "C";
             }
-            else if(x1.state == 1){
-                if(x3.state == 0)
-                    x3.state = 1;
-                else x3.state = 0;
-                x1.state = 1;
+            else if(x1.state == "R"){
+                if(x3.state == "L")
+                    x3.state = "R";
+                else x3.state = "L";
+                x1.state = "R";
             }
-            else if(x1.state == 2){
-                if(x4.state == 0)
-                    x4.state = 1;
-                else x4.state = 0;
-                x1.state = 0;
+            else if(x1.state == "C"){
+                if(x4.state == "L")
+                    x4.state = "R";
+                else x4.state = "L";
+                x1.state = "L";
             }
         }
         cout << x1.state << x2.state << x3.state << x4.state << "->";
     }
     cout << x1.state << x2.state << x3.state << x4.state <<endl;
-    if(x1.state == 0 && x2.state == 0)
+    if(x1.state == "L" && x2.state == "L")
         final = "B: ACCEPT";
-    else if(x1.state == 2 && x3.state == 1)
+    else if(x1.state == "C" && x3.state == "R")
         final = "D: ACCEPT";
-    else if(x1.state == 1 && x4.state == 0)
+    else if(x1.state == "R" && x4.state == "L")
         final = "D: ACCEPT";
-    else
-      final = "REJECT: Either State C or E";
+    else if(x1.state == "L" && x2.state == "R")
+      final = "C: Reject";
+    else if(x1.state == "R" && x2.state == "R")
+      final = "E: Reject";
+    else if(x1.state == "C" && x2.state == "R")
+      final = "C: Reject";
     cout << final << endl;
 }
